70e479897c4d421bd42aa593431338e666173f5e
Maria Bloshenko <m16091987m@gmail.com>
2022-02-14 18:36:22 +0200

    First commit

--- MyScript.js
+++ MyScript.js
@@ -1,4 +1,10 @@
-// Такой стиль не используется
+
 console.log('Start Script')
-alert('Собщение')
+//alert('Собщение') // Такой стиль не используется
+
+//let userName = prompt("Enter U Name", 'Mary')
+//console.log('user enter' + userName)
+// Ход скрипта останавливаетс, пока пользователь не введет информацию в поле
+
 console.log('End Script')
+
